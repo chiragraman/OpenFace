419b3e1e199f08ebfb8a349cfdf9f5c023fa5321
Chirag Raman <chirag.raman@gmail.com>
2016-11-30 11:08:41 -0500

    Add VisualFeatureExtractor

---
+++ exe/InMindDemo/src/visual_feature_extractor.cpp
@@ -0,0 +1,28 @@
+/**
+* @Author: Chirag Raman <chirag>
+* @Date:   2016-11-29T14:25:00-05:00
+* @Email:  chirag.raman@gmail.com
+* @Last modified by:   chirag
+* @Last modified time: 2016-11-30T09:40:25-05:00
+* @License: Copyright (C) 2016 Multicomp Lab. All rights reserved.
+*/
+
+#ifndef EXE_INMIND_DEMO_VISUAL_FEATURE_EXTRACTOR_H_
+#define EXE_INMIND_DEMO_VISUAL_FEATURE_EXTRACTOR_H_
+
+namespace inmind {
+
+class VisualFeatureExtractor {
+ public:
+     VisualFeatureExtractor();
+     virtual ~VisualFeatureExtractor ();
+ private:
+     /* data */
+
+
+};
+
+} /* inmind */
+
+
+#endif /* end of include guard: EXE_INMIND_DEMO_VISUAL_FEATURE_EXTRACTOR_H_ */
---
+++ exe/InMindDemo/src/visual_feature_extractor.h
@@ -0,0 +1,8 @@
+/**
+* @Author: Chirag Raman <chirag>
+* @Date:   2016-11-29T14:25:14-05:00
+* @Email:  chirag.raman@gmail.com
+* @Last modified by:   chirag
+* @Last modified time: 2016-11-29T14:32:20-05:00
+* @License: Copyright (C) 2016 Multicomp Lab. All rights reserved.
+*/
