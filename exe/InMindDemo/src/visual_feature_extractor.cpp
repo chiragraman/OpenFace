c97a6c49fe557529bd8bc703b6aaa9f43203872f
Chirag Raman <chirag.raman@gmail.com>
2016-12-07 13:53:15 -0500

    Add includes

--- exe/InMindDemo/src/visual_feature_extractor.cpp
+++ exe/InMindDemo/src/visual_feature_extractor.cpp
@@ -3,11 +3,18 @@
 * @Date:   2016-11-29T14:25:00-05:00
 * @Email:  chirag.raman@gmail.com
 * @Last modified by:   chirag
-* @Last modified time: 2016-12-07T13:46:54-05:00
+* @Last modified time: 2016-12-07T13:52:20-05:00
 * @License: Copyright (C) 2016 Multicomp Lab. All rights reserved.
 */
 
 #include "visual_feature_extractor.h"
+
+#include "LandmarkCoreIncludes.h"
+
+#include <Face_utils.h>
+#include <FaceAnalyser.h>
+#include <GazeEstimation.h>
+
 
 namespace inmind {
 
