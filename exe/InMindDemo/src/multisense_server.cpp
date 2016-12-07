b69921024a0019b2ce875c19541e03a346c7627d
Chirag Raman <chirag.raman@gmail.com>
2016-12-07 13:52:55 -0500

    Add include

--- exe/InMindDemo/src/multisense_server.cpp
+++ exe/InMindDemo/src/multisense_server.cpp
@@ -3,6 +3,8 @@
 * @Date:   2016-07-29T14:35:39-04:00
 * @Email:  chirag.raman@gmail.com
 * @Last modified by:   chirag
-* @Last modified time: 2016-07-29T14:36:00-04:00
+* @Last modified time: 2016-12-07T13:42:03-05:00
 * @License: Copyright (C) 2016 Multicomp Lab. All rights reserved.
 */
+
+#include "multisense_server.h"
