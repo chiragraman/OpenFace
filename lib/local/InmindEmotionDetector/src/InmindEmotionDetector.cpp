848b5c6e9b65c735766f15c2ffafe37b7e065447
Chirag Raman <chirag.raman@gmail.com>
2016-12-08 19:55:17 -0500

    Remove redundant lines

--- lib/local/InmindEmotionDetector/src/InmindEmotionDetector.cpp
+++ lib/local/InmindEmotionDetector/src/InmindEmotionDetector.cpp
@@ -3,11 +3,9 @@
 * @Date:   2016-11-04T11:11:49-04:00
 * @Email:  chirag.raman@gmail.com
 * @Last modified by:   chirag
-* @Last modified time: 2016-12-08T19:40:12-05:00
+* @Last modified time: 2016-12-08T19:54:53-05:00
 * @License: Copyright (C) 2016 Multicomp Lab. All rights reserved.
 */
-
-
 
 #include "InmindEmotionDetector.h"
 
